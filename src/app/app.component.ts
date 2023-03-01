7008224d8bd6a280d94c506670f1b1bbfff1cb5c
jsnik <js.saba999@gmail.com>
2023-03-01 05:32:35 -0800

    set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,15 @@ export class AppComponent implements OnInit, AfterViewInit{
   public renderer: THREE.WebGLRenderer;
 
   ngOnInit() {
-    this.scene = new THREE.Scene();
-    // Create the camera
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    this.camera.position.z = 5;
-
-    // Create the renderer
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.renderer.domElement);
+    // this.scene = new THREE.Scene();
+    // // Create the camera
+    // this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    // this.camera.position.z = 5;
+    //
+    // // Create the renderer
+    // this.renderer = new THREE.WebGLRenderer();
+    // this.renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild(this.renderer.domElement);
   }
 
   ngAfterViewInit() {
@@ -33,41 +33,34 @@ export class AppComponent implements OnInit, AfterViewInit{
     // }, undefined, (error: any) => {
     //   console.error(error);
     // });
-    const loader = new GLTFLoader();
-    loader.load('../assets/EPISODE-FBX.gltf', (gltf: any) => {
-      // Add the model to the scene
-      this.scene.add(gltf.scene);
-    }, undefined, (error: any) => {
-      console.error(error);
-    });
-
-    // Add an ambient light to the scene
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    this.scene.add(ambientLight);
-
-    // Add a point light to the scene
-    const pointLight = new THREE.PointLight(0xffffff, 0.5);
-    pointLight.position.set(0, 10, 10);
-    this.scene.add(pointLight);
-
-    // Adjust the camera position and lookAt target
-    this.camera.position.set(0, 0, 10);
-    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-    setTimeout(() => {
-      this.animate();
-    }, )
+    //
+    // // Add an ambient light to the scene
+    // const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    // this.scene.add(ambientLight);
+    //
+    // // Add a point light to the scene
+    // const pointLight = new THREE.PointLight(0xffffff, 0.5);
+    // pointLight.position.set(0, 10, 10);
+    // this.scene.add(pointLight);
+    //
+    // // Adjust the camera position and lookAt target
+    // this.camera.position.set(0, 0, 0);
+    // this.camera.lookAt(new THREE.Vector3(4, 5, 10));
+    //
+    // setTimeout(() => {
+    //   this.animate();
+    // }, )
   }
 
-  animate() {
-    requestAnimationFrame(() => this.animate());
-
-    // Update the scene
-    this.scene.rotation.y += 0.01;
-
-    // Render the scene
-    this.renderer.render(this.scene, this.camera);
-  }
+  // animate() {
+  //   requestAnimationFrame(() => this.animate());
+  //
+  //   // Update the scene
+  //   this.scene.rotation.y += 0.01;
+  //
+  //   // Render the scene
+  //   this.renderer.render(this.scene, this.camera);
+  // }
 
   constructor() {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HotelViewComponent } from './main/hotel-view/hotel-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HotelViewComponent
   ],
   imports: [
     BrowserModule,
